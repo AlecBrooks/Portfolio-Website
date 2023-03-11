3d4be14bbb77ca9e4b27adb1359a98b91801bdc3
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-10 16:43:10 -0800

    js

---
+++ src/js/blog.js
@@ -0,0 +1,14 @@
+fetch('/src/json/blog-posts.json')
+  .then(response => response.json())
+  .then(data => {
+    const ul = document.querySelector('ul');
+    data.blogPosts.forEach(post => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href = '#';
+      a.textContent = post.title;
+      li.appendChild(a);
+      ul.appendChild(li);
+    });
+  })
+  .catch(error => console.error(error));
