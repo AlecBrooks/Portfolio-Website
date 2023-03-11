502287499805154064ba6d5edf0ea51926e315e9
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-10 17:03:13 -0800

    1

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,18 +1,35 @@
-fetch("src/json/blog-posts.json")
+fetch('/src/json/blog-posts.json')
   .then(response => response.json())
   .then(data => {
-    const posts = data.blogPosts;
-    const postsContainer = document.getElementById("posts-container");
-    posts.forEach(post => {
-      const postDiv = document.createElement("li");
-      postDiv.className = "post";
-      postDiv.innerHTML = `
-        <h2>${post.title}</h2>
-        <p>${post.datePublished}</p>
-        <p>${post.content}</p>
-        <img src="${post.image.url}" alt="${post.image.altText}">
-      `;
-      postsContainer.appendChild(postDiv);
+    const blogPosts = data.blogPosts;
+    const blogPostsContainer = document.getElementById('blog-posts-container');
+
+    blogPosts.forEach(post => {
+      const postItem = document.createElement('li');
+      postItem.classList.add('post-item');
+
+      const postTitle = document.createElement('h2');
+      postTitle.classList.add('post-title');
+      postTitle.textContent = post.title;
+
+      const postDate = document.createElement('p');
+      postDate.classList.add('post-date');
+      postDate.textContent = post.datePublished;
+
+      const postContent = document.createElement('p');
+      postContent.classList.add('post-content');
+      postContent.textContent = post.content;
+
+      const postImage = document.createElement('img');
+      postImage.classList.add('post-image');
+      postImage.src = post.image.url;
+      postImage.alt = post.image.altText;
+
+      postItem.appendChild(postTitle);
+      postItem.appendChild(postDate);
+      postItem.appendChild(postContent);
+      postItem.appendChild(postImage);
+      blogPostsContainer.appendChild(postItem);
     });
   })
   .catch(error => console.error(error));
