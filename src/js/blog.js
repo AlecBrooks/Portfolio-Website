b78760bf96c26900b9c2ede8f22ede53466c0564
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-10 16:55:49 -0800

    123

--- src/js/blog.js
+++ src/js/blog.js
@@ -4,10 +4,19 @@ fetch('src/json/blog-posts.json')
     const ul = document.getElementById('blog-posts');
     data.blogPosts.forEach(post => {
       const li = document.createElement('li');
-      const a = document.createElement('a');
-      a.href = '#';
-      a.textContent = post.title;
-      li.appendChild(a);
+      const h2 = document.createElement('h2');
+      const date = document.createElement('p');
+      const content = document.createElement('p');
+      const img = document.createElement('img');
+      h2.textContent = post.title;
+      date.textContent = post.datePublished;
+      content.textContent = post.content;
+      img.src = post.image.url;
+      img.alt = post.image.altText;
+      li.appendChild(h2);
+      li.appendChild(date);
+      li.appendChild(content);
+      li.appendChild(img);
       ul.appendChild(li);
     });
   })
