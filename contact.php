70a64cec81ac80e455d6b69f3f4c85ace71f819f
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-12 16:31:45 -0700

    gf

--- contact.php
+++ contact.php
@@ -22,27 +22,19 @@
     </header>
     <!-- Main Content -->
     <main class = "contactmeBox">
-
-        <ul class="ContactUl">
+        <ul>
             <li class="post">
-                <form action="" method="post">
-
+                <form  method="POST">
                     <label for="name">Name</label>
                     <input type="text" id="name" name="name" required><br>
-
                     <label for="email">Email</label>
                     <input type="email" id="email" name="email" required><br>
-
                     <label for="subject">Subject</label>
                     <input type="text" id="subject" name="subject" required><br>
-
                     <label for="message">Message</label>
                     <textarea id="message" name="message" rows="15" cols="70"></textarea></textarea><br>
-
                     <input type="submit" name="submit" value="Submit">
-
                     <?php echo $alert; ?>
-
                 </form>
             </li>
         </ul>
