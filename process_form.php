61c36d21072e5c594319d27d9886e451ac8d01a6
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-12 11:43:30 -0700

    update php

--- process_form.php
+++ process_form.php
@@ -4,16 +4,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
-    // Use headers to set the From, Reply-To, and MIME type
+    // Use headers to set the Reply-To and MIME type
     $headers = array(
-        'From: ' . $from,
         'Reply-To: ' . $from,
         'MIME-Version: 1.0',
-        'Content-type: text/html; charset=UTF-8'
+        'Content-type: text/html; charset=UTF-8',
+        'From: ' . $from
     );
 
-    // Send the email using the mail() function
-    if (mail('alec@brooksdev.org', $subject, $message, implode("\r\n", $headers))) {
+    // Set the additional parameters for sendmail, including the -f flag with the From address
+    $additional_parameters = '-f ' . $from;
+
+    // Send the email using the mail() function with the additional parameters
+    if (mail('alec@brooksdev.org', $subject, $message, implode("\r\n", $headers), $additional_parameters)) {
         echo 'Email sent successfully.';
     } else {
         echo 'Email could not be sent.';
