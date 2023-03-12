71d7861da28ec82e519a5c51f537e97a2d5a63dc
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-12 04:32:12 -0700

    added portfolio json system

---
+++ src/js/Portfolio.js
@@ -0,0 +1,44 @@
+fetch('/src/json/portfolio-posts.json')
+  .then(response => response.json())
+  .then(data => {
+    const blogPosts = data.blogPosts;
+    const blogPostList = document.getElementById("blog-post-list");
+
+    blogPosts.forEach(post => {
+      const postItem = document.createElement("li");
+      postItem.classList.add("posts");
+
+      const postTitle = document.createElement("h2");
+      postTitle.innerText = post.title;
+
+      const postDate = document.createElement("p");
+      postDate.innerText = new Date(post.datePublished).toLocaleDateString();
+
+      const postContent = document.createElement("section");
+
+      const postDetails = document.createElement("div");
+      postDetails.classList.add("postDetails");
+      postDetails.appendChild(postTitle);
+      postDetails.appendChild(postDate);
+      
+      postContent.appendChild(postDetails); // move postDetails to the top
+
+      post.content.forEach(p => {
+        const para = document.createElement("p");
+        para.innerText = p;
+        postContent.appendChild(para);
+      });
+
+      const postImage = document.createElement("img");
+      postImage.classList.add("postImage");
+      postImage.setAttribute("src", post.image.url);
+      postImage.setAttribute("alt", post.image.altText);
+
+      postItem.appendChild(postContent);
+      postItem.appendChild(postImage); // move postImage to the end
+      blogPostList.appendChild(postItem);
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
