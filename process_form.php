ff3c266164b49230e5649073d74a2edf7bb03d1a
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-12 04:46:26 -0700

    php

--- process_form.php
+++ process_form.php
@@ -1,21 +1,12 @@
 <?php
-
-// Check if form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  
-  // Collect form data
-  $to = "alec@brooksdev.org"; // Updated recipient email address
-  $subject = $_POST["subject"];
-  $message = $_POST["message"];
-  $headers = "From: " . $_POST["email"] . "\r\n" .
-             "Reply-To: " . $_POST["email"] . "\r\n" .
-             "X-Mailer: PHP/" . phpversion();
-  
-  // Send email using PHP mail function
-  if (mail($to, $subject, $message, $headers)) {
-    echo "Email sent successfully!";
-  } else {
-    echo "Email sending failed.";
-  }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $to = "alec@brooksdev.org";
+    $headers = "From: $email";
+    $message_body = "Name: $name\nEmail: $email\nSubject: $subject\n\n$message";
+    mail($to, $subject, $message_body, $headers);
 }
 ?>
