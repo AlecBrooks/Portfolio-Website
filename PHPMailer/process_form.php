350a994172f7c0c81b83c01b498562e3d9c9bf18
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-12 14:40:49 -0700

    gf

--- PHPMailer/process_form.php
+++ PHPMailer/process_form.php
@@ -1,14 +1,13 @@
-<? php
-
+<?php
 use PHPMailer\PHPMailer\PHPMailer;
 
-required_once 'src/Exception.php';
-required_once 'src/PHPMailer.php';
-required_once 'src/SMTP.php';
+require_once 'src/Exception.php';
+require_once 'src/PHPMailer.php';
+require_once 'src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
-@alert = '';
+$alert = '';
 
 if(isset($_POST['submit'])){
     $name = $_POST['name'];
@@ -22,23 +21,22 @@ if(isset($_POST['submit'])){
         $mail->SMTPAuth = true;
         $mail->Username = 'alec@brooksdev.org';
         $mail->Password = 'fkqbphjbewmaledg';
-        $mail->SMTPSecure = "tls";
-        $mail->Post = '587';
+        $mail->SMTPSecure = "ssl";
+        $mail->Port = '465';
 
         $mail->setFrom("Alec@brooksdev.org");
         $mail->addAddress("Alec@brooksdev.org");
 
         $mail->isHTML(true);
 
-        $mail->Subject = 'Email From: ' . $name
-        $mail->$Body = "Name: $name<br>Email: $email<br>Subject: $subject<br>$message";
+        $mail->Subject = 'Email From: ' . $name;
+        $mail->Body = "Name: $name<br>Email: $email<br>Subject: $subject<br>$message";
 
-        $mail->send()
+        $mail->send();
         $alert = "<div class='alert-success'></div><span>Message Sent!</span>";
 
     } catch(Exception $e){
-        $alert = "<div class='alert-success'></div><span>Message Sent!</span>";
+        $alert = "<div class='alert-danger'></div><span>Message could not be sent. Mailer Error: {$mail->ErrorInfo}</span>";
     }
 }
-
 ?>
