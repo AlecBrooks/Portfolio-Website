a7d6cfea8e62d1adac4baf5f849586909e1106af
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-12 18:19:48 -0700

    hg

--- src/js/colors.js
+++ src/js/colors.js
@@ -1,7 +1,8 @@
 var link = document.createElement("link");
 //============================================
-//link.href = "\src\colors\Gatsby.css";
-link.href = "\src\colors\80s.css";
+//link.href = "/src/colors/Gatsby.css";
+link.href = "/src/colors/80s.css";
+link.href = "/src/colors/techy.css";
 //============================================
 link.rel = "stylesheet";
 document.head.appendChild(link);
