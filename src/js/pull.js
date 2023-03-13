abbbaae03066aea651448195954a46c86c7d995c
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-12 23:17:04 -0700

    re

--- src/js/pull.js
+++ src/js/pull.js
@@ -13,21 +13,21 @@ function PageVariable(url) {
 }
 
 function embedHTML() {
-    const url = localStorage.getItem("PageVar");
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.responseType = "document";
-    xhr.onload = function () {
-      const responseHTML = xhr.response.documentElement;
-      const mainElement = responseHTML.querySelector("main");
-      const currentMainElement = document.querySelector("main");
-      if (mainElement && currentMainElement) {
-        currentMainElement.innerHTML = mainElement.innerHTML;
-      } else {
-        console.error("Could not find the <main> element in either the response HTML or the current document.");
-      }
-    };
-    xhr.send();
-    let myVariable = localStorage.getItem('PageVar');
-    localStorage.removeItem('PageVar');
-  }
+  const url = localStorage.getItem("PageVar");
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.responseType = "document";
+  xhr.onload = function () {
+    const responseHTML = xhr.response.documentElement;
+    const myContentElement = responseHTML.querySelector("#MyContent");
+    const currentMyContentElement = document.querySelector("#MyContent");
+    if (myContentElement && currentMyContentElement) {
+      currentMyContentElement.innerHTML = myContentElement.innerHTML;
+    } else {
+      console.error("Could not find the #MyContent element in either the response HTML or the current document.");
+    }
+  };
+  xhr.send();
+  let myVariable = localStorage.getItem('PageVar');
+  localStorage.removeItem('PageVar');
+}
