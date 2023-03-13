4b52c8843ba284ddba77b7bb9fe394ed2bc78ee1
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-12 17:52:58 -0700

    gfg

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
     <title>About Me | Alec Brooks</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="src/style.css">
+    <link rel="stylesheet" type="text/css" href="src/main.css">
     <link rel="icon" href="">
 </head>
 <body>
