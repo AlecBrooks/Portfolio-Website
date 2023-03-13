a5e62fdf2f608c76a4e3598fe6fa110e83d2c46a
AlecBrooks <126025860+AlecBrooks@users.noreply.github.com>
2023-03-12 22:18:11 -0700

    fd

---
+++ src/js/pull.js
@@ -0,0 +1,19 @@
+function embedHTML() {
+    const xhr = new XMLHttpRequest();
+    const url = "/content/intro.html";
+    xhr.open("GET", url, true);
+    xhr.responseType = "document";
+    xhr.onload = function () {
+      const responseHTML = xhr.response.documentElement;
+      const mainElement = responseHTML.querySelector("main");
+      const currentMainElement = document.querySelector("main");
+      if (mainElement && currentMainElement) {
+        currentMainElement.innerHTML = mainElement.innerHTML;
+      } else {
+        console.error("Could not find the <main> element in either the response HTML or the current document.");
+      }
+    };
+    xhr.send();
+  }
+  
+  embedHTML();	
