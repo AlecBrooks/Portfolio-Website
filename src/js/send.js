3dd3086e59bf49c941df57a3623425e3abc13154
Alec Brooks <126025860+AlecBrooks@users.noreply.github.com>
2023-06-21 03:37:29 -0700

    contact page

--- src/js/send.js
+++ src/js/send.js
@@ -9,5 +9,34 @@ function sendForm() {
     console.log('Subject:', subject);
     console.log('Message:', message);
   
+    var webhookUrl = 'YOUR_DISCORD_WEBHOOK_URL';
+    var payload = {
+      content: `
+        New message received from the contact form:
+        Name: ${name}
+        Email: ${email}
+        Subject: ${subject}
+        Message: ${message}
+      `
+    };
+  
+    fetch(webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+      .then(response => {
+        if (response.ok) {
+          // Success! The message has been sent to your Discord server.
+        } else {
+          // Error handling if the request fails
+        }
+      })
+      .catch(error => {
+        // Error handling for network or other issues
+      });
+  
     // You can add further code here to process or display the form data as needed
   }
