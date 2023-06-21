5fb6c6ce45f91dbae56b20c38d498b8fb6ea21a7
Alec Brooks <126025860+AlecBrooks@users.noreply.github.com>
2023-06-21 03:49:48 -0700

    contact page

--- src/js/send.js
+++ src/js/send.js
@@ -1,14 +1,10 @@
-function sendForm() {
+function sendForm(event) {
     event.preventDefault();
+  
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var subject = document.getElementById('subject').value;
     var message = document.getElementById('message').value;
-  
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Subject:', subject);
-    console.log('Message:', message);
   
     var webhookUrl = 'https://discord.com/api/webhooks/1121026381283864657/GTJ1LIgD34Q5OPwRtPgrfUcOGkfEGq6N5AO4QAPI8dib1t-Hg0FSteUyzKBQPGnc9B2A';
     var payload = {
@@ -31,13 +27,31 @@ function sendForm() {
       .then(response => {
         if (response.ok) {
           // Success! The message has been sent to your Discord server.
+          showNotification('Message sent successfully!');
+          clearForm();
         } else {
           // Error handling if the request fails
+          showNotification('Failed to send message.');
         }
       })
       .catch(error => {
         // Error handling for network or other issues
+        showNotification('Failed to send message.');
       });
+  }
   
-    // You can add further code here to process or display the form data as needed
+  function showNotification(message) {
+    var notification = document.getElementById('notification');
+    notification.textContent = message;
+    notification.style.display = 'block';
+    setTimeout(function() {
+      notification.style.display = 'none';
+    }, 3000);
+  }
+  
+  function clearForm() {
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('subject').value = '';
+    document.getElementById('message').value = '';
   }
