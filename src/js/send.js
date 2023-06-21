c89bb775efcb4149012bb9bd8260b3643fa37630
Alec Brooks <126025860+AlecBrooks@users.noreply.github.com>
2023-06-21 04:34:07 -0700

    contact page

--- src/js/send.js
+++ src/js/send.js
@@ -7,7 +7,7 @@ function sendForm(event) {
   
     var webhookUrl = 'https://discord.com/api/webhooks/1121026381283864657/GTJ1LIgD34Q5OPwRtPgrfUcOGkfEGq6N5AO4QAPI8dib1t-Hg0FSteUyzKBQPGnc9B2A';
     var timestamp = new Date().toISOString();
-    var formattedMessage = `**Message**:\n\n${message}`;
+    var formattedMessage = `\n\n${message}`;
   
     var payload = {
       content: '',
